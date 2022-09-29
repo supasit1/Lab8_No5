c0ec633f06af0c0af15b024395b1945e296c2e7e
Supasit Chinsa <109809925+supasit1@users.noreply.github.com>
2022-09-29 15:11:27 +0700

    Update Lab8_No5_detail.php

--- Lab8_No5_detail.php
+++ Lab8_No5_detail.php
@@ -4,7 +4,7 @@
     <?php
     $pdo = new PDO("mysql:host=localhost;dbname=blueshop;charset=utf8", "root", "");
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $pdo->prepare("SELECT * FROM member WHERE username");
+    $stmt = $pdo->prepare("SELECT * FROM member WHERE username LIKE ?");
     $stmt->bindParam(1,$_GET["username"]);
     $stmt->execute();
     $row = $stmt->fetch();
